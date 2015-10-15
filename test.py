b3be4147e96648b8c550e62dc528b7d16ef73d2d
Karl-Mattias <Karl-Mattias@users.noreply.github.com>
2015-10-15 11:32:35 +0300

    Commit to send changes in driving logic to robot.

---
+++ test.py
@@ -0,0 +1,9 @@
+from motor_controller import MotorController
+
+__author__ = 'Karl'
+
+motor_controller = MotorController()
+
+motor_controller.move_back_wheel(30)
+motor_controller.move_left_wheel(30)
+motor_controller.move_right_wheel(30)
