a82c5fc7c1a9e4d070eea3e1addc403a72067043
Karl-Mattias <Karl-Mattias@users.noreply.github.com>
2015-10-25 22:54:01 +0200

    Send changes to robot

--- turn_to_goal.py
+++ turn_to_goal.py
@@ -15,20 +15,20 @@ class TurnToGoal:
 		coordinates = self.get_gate_coordinates.get_coordinates()
 		while coordinates == -1:
 			self.motor_controller.stop()
-			self.motor_controller.move_back_wheel(100)
+			self.motor_controller.move_back_wheel(60)
 			coordinates = self.get_gate_coordinates.get_coordinates()
 
-		x = coordinates[0]
-
 		while True:
+			coordinates = self.get_gate_coordinates.get_coordinates()
+			x = coordinates[0]
 			if x < 300:
 				self.motor_controller.stop()
-				self.motor_controller.move_back_wheel(100)
+				self.motor_controller.move_back_wheel(60)
+			elif x > 350:
+				self.motor_controller.stop()
+				self.motor_controller.move_back_wheel(-60)
 			else:
 				break
-			'''elif x > 350:
-				self.motor_controller.stop()
-				self.motor_controller.move_back_wheel(-30)'''
 
 		self.motor_controller.stop()
 		self.motor_controller.move_right_wheel(100)
