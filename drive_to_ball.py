31e45385b5168460ea9c5ff03a9f7cdfdd5e533c
gakola <gakola@git.ttu.ee>
2015-11-01 15:48:48 +0200

    Wrapped drive_to_ball into a class, created the referee class, added extra settings parameters to the setting dictionary

--- drive_to_ball.py
+++ drive_to_ball.py
@@ -1,25 +1,25 @@
-from get_coordinates import GetCoordinates
+﻿from get_coordinates import GetCoordinates
 from drive_towards import DriveTowards
 from turn_to_goal import TurnToGoal
 
 __author__ = 'Karl'
 
+class DriveController(object):
 
-def drive_to_ball():
+    def drive_to_ball():
+        get_ball_coordinates = GetCoordinates("ball")
+        driver = DriveTowards()
+        to_goal = TurnToGoal()
 
-	get_ball_coordinates = GetCoordinates("ball")
-	driver = DriveTowards()
-	to_goal = TurnToGoal()
+        while True:
+            coordinates = get_ball_coordinates.get_coordinates()
+            driver.drive(coordinates)
+            if coordinates != -1:
+                print("Y:", coordinates[1])
+                if coordinates[1] > 450:
+                    print("near ball")
+                    to_goal.turn()
+                    break
 
-	while True:
-		coordinates = get_ball_coordinates.get_coordinates()
-		driver.drive(coordinates)
 
-		if coordinates != -1:
-			print("Y:", coordinates[1])
-			if coordinates[1] > 450:
-				print("near ball")
-				to_goal.turn()
-				break
-
-drive_to_ball()
+#drive_to_ball()
---
+++ referee.py
@@ -0,0 +1,65 @@
+﻿import time, os, serial
+from settings import BoltSettings
+from drive_to_ball import DriveController
+
+__author__ = 'Gabriel'
+
+class RefereeController(object):
+    def __init__(self):
+        self.serialChannel = serial.Serial("/dev/ttyACM0", 9600)
+        self.boltSettings = BoltSettings.read_dict()
+        self.CharCounter = 0
+        self.initChar = self.boltSettings.initialCharSignal
+        self.listening = False
+        self.respond = False
+        
+    def writeAckString (self):
+        self.initChar+self.boltSettings.playingField+self.boltSettings.robotID+self.boltSettings.ackMsg
+            
+    def listen(self):
+        initChar = self.initChar
+        self.CharCounter += 1
+        charSignal = self.serialChannel.read().decode()
+        while 1:
+            if charSignal == initChar:
+                self.CharCounter = 0
+                self.listening = True
+                continue
+            if not self.listening:
+                continue # wait for next a
+            if self.CharCounter == 1 and charSignal != self.boltSettings.playingField:
+                self.listening = False
+            if self.CharCounter == 2 and charSignal != self.boltSettings.robotID and charSignal != self.boltSettings.allRobotsChar:
+                self.listening = False
+            if self.CharCounter == 2 and charSignal == self.boltSettings.robotID:
+                self.respond = True;
+            if self.CharCounter == 2 and charSignal == self.boltSettings.allRobotsChar:
+                self.respond = False    
+            if self.CharCounter == 2 and self.listening == True:
+                msg = ""
+                for self.CharCounter in range (3, 12):
+                    charSignal = self.serialChannel.read().decode()
+                    if charSignal == initChar:
+                        self.CharCounter = 0
+                        break
+                    if charSignal == "-":
+                        self.listening = False
+                        break
+                    msg += charSignal
+                    if msg in ["START", "STOP"]:
+                        print(msg)
+                        if self.respond == True:
+                            self.serialChannel.write((self.writeAckString).encode())
+
+                        #We robot will stop any where
+                        while msg != "STOP":
+                            DriveController.drive_to_ball()
+                            
+   
+            
+                
+
+
+
+
+
