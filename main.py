c3135893a2430825c6c3ec7a4f72495ac0e22ddb
Karl-Mattias <Karl-Mattias@users.noreply.github.com>
2015-11-02 17:31:56 +0200

    Send changes to robot

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ td = threading.Thread(target=RefereeController.listen)
 td.start()
 
 while 1:
-    f = open('referee.command','r')
+    f = open('referee.command', 'r')
     play_on = eval(f.readline())
 
-    if (play_on == True):
+    if play_on:
         DriveController.drive_to_ball()
 
