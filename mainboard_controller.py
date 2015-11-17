08fa637944b899899d47d054a2ec6e23c3ea3408
Karl-Mattias <Karl-Mattias@users.noreply.github.com>
2015-11-17 21:11:12 +0200

    push changes to robot

--- mainboard_controller.py
+++ mainboard_controller.py
@@ -7,8 +7,8 @@ __author__ = 'Karl'
 class MainBoardController:
 
 	def __init__(self):
-		os.chmod("/dev/ttyACM3", 755)  # set permissions to read serial
-		self.mainboard = serial.Serial("/dev/ttyACM3", 19200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, 3)
+		os.chmod("/dev/ttyACM4", 755)  # set permissions to read serial
+		self.mainboard = serial.Serial("/dev/ttyACM4", 19200, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE, 3)
 
 	def ping(self):
 		self.mainboard.write("p")
