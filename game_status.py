296f0ea0d3c9a4b108f55a1561b288a768ac3c9c
Karl-Mattias <Karl-Mattias@users.noreply.github.com>
2015-11-17 20:53:30 +0200

    push changes to robot

--- game_status.py
+++ game_status.py
@@ -2,11 +2,12 @@ class GameStatus(object):
 
 	@staticmethod
 	def status():
+		last_status = False
 		try:
 			command_file = open('referee.command', 'r')
 			status = eval(command_file.readline())
 			command_file.close()
 			last_status = status
 			return status
-		except EOFError:
+		except SyntaxError: # when file is empty during writing
 			return last_status
