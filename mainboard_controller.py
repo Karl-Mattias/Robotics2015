d3bae7a6e7a2bbbc28eb3691b24f4323bdeb2014
Karl-Mattias <Karl-Mattias@users.noreply.github.com>
2015-11-18 19:34:55 +0200

    send changes to robot

--- mainboard_controller.py
+++ mainboard_controller.py
@@ -15,7 +15,7 @@ class MainBoardController:
 		self.mainboard.write("p\n")
 
 	def start_dribbler(self):
-		self.mainboard.write("dm50\n")
+		self.mainboard.write("dm100\n")
 
 	def stop_dribbler(self):
 		self.mainboard.write("dm0\n")
